fb49182ef6a969ba223aef53e413950f892c2d8a
N0ku <lucielecouturier2@gmail.com>
2022-09-27 11:35:01 +0200

    Test 1 query parameters

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-import User
 
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-new_user = User.User("Michel", 47, "18 january 1975", 10)
-
-User.get_user_infos(new_user)
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item

9646d466399c2f7c99aafef07a318cb286c80c6e
N0ku <lucielecouturier2@gmail.com>
2022-09-27 11:07:18 +0200

    test path parameters

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ import User
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-new_user = User.User("Michel", 47, "18 janvier 1975", 10)
+new_user = User.User("Michel", 47, "18 january 1975", 10)
 
 User.get_user_infos(new_user)

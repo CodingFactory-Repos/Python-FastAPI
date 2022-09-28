45ca0a9612d4b75a19b73ee7dbf43aeceaafe5a5
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-09-28 16:22:52 +0200

    🎆 - Add possibility to view and add a image

--- routes/image.py
+++ routes/image.py
@@ -10,3 +10,20 @@ image = APIRouter()
 @image.get("/")
 async def read_images():
     return conn.execute(images.select()).fetchall()
+
+
+@image.get("/{id}")
+async def read_image(id: int):
+    return conn.execute(images.select().where(images.c.i_id == id)).first()
+
+
+@image.post("/{id}")
+async def create_image(id: int, file: UploadFile):
+    # Check if file is an image
+    if file.content_type.startswith("image/"):
+        # Convert image to base64
+        image = base64.b64encode(await file.read()).decode("utf-8")
+
+        conn.execute(images.insert().values(i_id=id, i_image=image))
+
+        return {"status": 200, "message": "Image uploaded successfully", "data": conn.execute(images.select().where(images.c.i_id == id)).first()}
